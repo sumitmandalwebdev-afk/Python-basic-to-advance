32f01f396e44f98fcd545b02758906618d450e24
Sumit_Mandal <sumitmandal.webdev@gmail.com>
2026-09-24 19:58:35 +0530

    Add Chapter 1: Introduction to Python with sample program

---
+++ .dist/Chapter1.py
@@ -0,0 +1,17 @@
+'''
+                                 Chapter 1 
+                           Introduction to Python 
+                                
+                                 Questions 
+'''
+
+'Q1. Write a program that prints your name, college name and course on separate lines.'
+
+name = "Sumit_Mandal"
+college_name = "DR. KN MIET "
+course = "B.Tech"
+print(f"My name is {name}")
+print(f"My college is  {college_name}")
+print(f"Currently doing  {course}")
+
+'Q2. '
